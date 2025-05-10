856f67ab32d9b2350c1c7c6a2b2ef7863a4d9438
tejeoner34 <atejezar@gmail.com>
2025-05-10 17:07:00 +0900

    feat: add header component

---
+++ components/ui/header.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from './navigation-menu';
+import { ROUTES } from '@/lib/routing/routes';
+
+const Header = () => {
+  const links = [
+    {
+      name: 'Grammar',
+      href: ROUTES.grammar,
+    },
+    {
+      name: 'Kanji',
+      href: ROUTES.kaji,
+    },
+  ];
+  return (
+    <header>
+      <NavigationMenu>
+        <NavigationMenuList>
+          {links.map((link) => (
+            <NavigationMenuItem key={link.name}>
+              <Link href={link.href} legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  {link.name}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+};
+
+export default Header;
