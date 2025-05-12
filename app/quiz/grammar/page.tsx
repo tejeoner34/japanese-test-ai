9e3cda78bedfdd6e70e797287df13a5d1e487348
tejeoner34 <atejezar@gmail.com>
2025-05-12 17:49:25 +0900

    feat: add services methods and grammar page

--- app/quiz/grammar/page.tsx
+++ app/quiz/grammar/page.tsx
@@ -1,7 +1,24 @@
-export default function GrammarPage() {
+import { getGrammarQuiz } from '@/lib/services/quiz.service';
+
+export default async function GrammarPage() {
+  const questions = await getGrammarQuiz('N5');
+  console.log(questions);
+  if (!questions || questions.length === 0) return 'loading';
   return (
     <div>
       <h1>Grammar page</h1>
+      {questions.map((question) => (
+        <div key={question.questionText} className="mb-4">
+          <h2 className="text-lg font-bold">{question.questionText}</h2>
+          <ul className="list-disc pl-5">
+            {question.options.map((option, index) => (
+              <li key={index} className="text-gray-700">
+                {option.text}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ lib/services/quiz.service.ts
@@ -0,0 +1,17 @@
+import { Question } from '../models/quiz';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const getGrammarQuiz = async (level: string): Promise<Question[]> => {
+  return fetch(`${API_URL}quiz/grammar-quiz/?level=${level}`)
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((error) => console.error('Error fetching grammar quiz:', error));
+};
+
+export const getKanjiQuiz = async (level: string): Promise<Question[]> => {
+  return fetch(`${API_URL}/quiz/kanji-quiz/${level}`)
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((error) => console.error('Error fetching kanji quiz:', error));
+};
